b262c95e028d57206ba17cfb29b14ca35bfbdf53
Dharfa Fathir <dharfaf@gmail.com>
2025-10-19 17:14:33 +0700

    Feature: Add navbar, slider at homepage & slider catalog

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -2,10 +2,6 @@ from flask import Blueprint, render_template
 
 menu = Blueprint('menu', __name__)
 
-# @menu.route('/')
-# def menu_home():
-#     return render_template('index.html')
-
 @menu.route('/about-us')
-def menu_about():
+def about():
     return render_template('about.html')
