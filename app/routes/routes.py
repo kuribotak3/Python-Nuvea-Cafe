7f98e91aebc744fe1b4c5b1b2a2568fdab813d72
Dharfa Fathir <dharfaf@gmail.com>
2025-10-24 00:54:07 +0700

    Feature: Screen catalog menu

--- app/routes/routes.py
+++ app/routes/routes.py
@@ -5,3 +5,7 @@ menu = Blueprint('menu', __name__)
 @menu.route('/about-us')
 def about():
     return render_template('about/index.html')
+
+@menu.route('/catalog-menu')
+def catalog():
+    return render_template('catalog/index.html')
